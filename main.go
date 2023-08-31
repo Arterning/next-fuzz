0a7c8eca7703c34aa13771ca87ebcfa5f6a7b242
Arterning <ning.huang.ridson@gmail.com>
2023-08-31 10:37:06 +0800

    add json serialize

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
+	"os"
 	"time"
 )
 
@@ -14,19 +16,55 @@ func main() {
 
 	log.Println("Starting simplefts")
 
-	start := time.Now()
-	docs, err := loadDocuments(dumpPath)
-	if err != nil {
-		log.Fatal(err)
+	var docs []document
+	_, err := os.Stat("doc.json")
+	if os.IsNotExist(err) {
+		//load documents
+		start := time.Now()
+		docs, err = loadDocuments(dumpPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Loaded %d documents in %v", len(docs), time.Since(start))
+
+		//store the docs
+		err = saveDocAsJSON(docs, "doc.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		docs, err = loadDocsFromJSON("doc.json")
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	log.Printf("Loaded %d documents in %v", len(docs), time.Since(start))
 
-	start = time.Now()
 	idx := make(index)
-	idx.add(docs)
-	log.Printf("Indexed %d documents in %v", len(docs), time.Since(start))
+	_, err = os.Stat("index.json")
+	if os.IsNotExist(err) {
+		//make index
+		start := time.Now()
+		idx.add(docs)
+		log.Printf("Indexed %d documents in %v", len(docs), time.Since(start))
 
-	start = time.Now()
+		//store the idx
+		err = saveIndexAsJSON(idx, "index.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		idx, err = loadIndexFromJSON("index.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	//do search
+	doSearch(idx, query, docs)
+}
+
+func doSearch(idx index, query string, docs []document) {
+	start := time.Now()
 	matchedIDs := idx.search(query)
 	log.Printf("Search found %d documents in %v", len(matchedIDs), time.Since(start))
 
@@ -34,4 +72,70 @@ func main() {
 		doc := docs[id]
 		log.Printf("%d\t%s\n", id, doc.Text)
 	}
+}
+
+func saveDocAsJSON(docs []document, filename string) error {
+	// 将文档数组序列化为 JSON 格式
+	jsonData, err := json.MarshalIndent(docs, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	// 将 JSON 数据写入文件
+	err = os.WriteFile(filename, jsonData, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadDocsFromJSON(filename string) ([]document, error) {
+	// 从文件中读取 JSON 数据
+	jsonData, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// 反序列化 JSON 数据到 document 结构体数组
+	var docs []document
+	err = json.Unmarshal(jsonData, &docs)
+	if err != nil {
+		return nil, err
+	}
+
+	return docs, nil
+}
+
+func saveIndexAsJSON(idx index, filename string) error {
+	// 将倒排索引序列化为 JSON 格式
+	jsonData, err := json.MarshalIndent(idx, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	// 将 JSON 数据写入文件
+	err = os.WriteFile(filename, jsonData, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadIndexFromJSON(filename string) (index, error) {
+	// 从文件中读取 JSON 数据
+	jsonData, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// 反序列化 JSON 数据到 index 类型的倒排索引
+	var idx index
+	err = json.Unmarshal(jsonData, &idx)
+	if err != nil {
+		return nil, err
+	}
+
+	return idx, nil
 }
--- tokenizer_test.go
+++ tokenizer_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -29,5 +31,19 @@ func TestTokenizer(t *testing.T) {
 		t.Run(tc.text, func(st *testing.T) {
 			assert.EqualValues(st, tc.tokens, tokenize(tc.text))
 		})
+	}
+}
+
+func TestFileExists(t *testing.T) {
+	filename := "doc.json"
+
+	// 使用 os.Stat 检查文件是否存在
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		fmt.Printf("File %s does not exist\n", filename)
+	} else if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Printf("File %s exists\n", filename)
 	}
 }
